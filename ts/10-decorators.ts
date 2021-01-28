6b4cbad14eb1a700e83fbf3b02ee072fde80c05c
Gabriel Godoy <gabrielgodoydev@gmail.com>
2021-01-27 23:02:17 -0300

    update decorators

--- ts/10-decorators.ts
+++ ts/10-decorators.ts
@@ -12,6 +12,7 @@
 function log(decoratorTarget: any) {
   console.log(`Constructor da classe sendo decorada: ${decoratorTarget}`)
 }
+
 @log
 class Bar {}
 
@@ -25,7 +26,8 @@ function loggerDecorator(prefix: string) {
 @loggerDecorator('Constructor da classe é: ')
 class Foo {}
 
-// Class decorator
+// CLASS DECORATOR
+// O decorator roda toda vez que criamos uma nova instância
 function setApiVersion(apiVersion: string) {
   // O constructor da class onde o decorator vai ser aplicado
   return (constructor: any) => {
@@ -35,20 +37,24 @@ function setApiVersion(apiVersion: string) {
     }
   }
 }
-// decorator - Anotar a versão da API
+// Decorator para anotar a versão da API
 @setApiVersion('1.0.0')
 class API {}
-// console.log(new API());
+console.log(new API())
 
 // PROPERTY DECORATOR
+// Decorator para ser aplicado numa propriedade da classe
 function minLength(length: number) {
   // target é o prototype da nossa classe
-  // key é o nome da propriedade
+  // key é o nome da propriedade, no caso 'title'
   return (target: any, key: string) => {
     // propriedade da classe
     let val = target[key]
 
+    // getter retorna o valor
     const getter = () => val
+
+    // Função que recebe o valor 'title' e seta
     const setter = (value: string) => {
       if (value.length < length) {
         console.log(`Error: você não pode criar ${key} menor que ${length}`)
@@ -65,7 +71,7 @@ function minLength(length: number) {
 }
 
 class Movie {
-  // validação - se tiver length menor que 5 - erro
+  // validação - se tiver length menor que 5 = erro
   @minLength(5)
   title: string
 
@@ -74,23 +80,31 @@ class Movie {
   }
 }
 const movie = new Movie('Interestellar')
-// console.log('Movie: ', movie.title);
+console.log('Movie: ', movie.title)
 
 // METHOD DECORATOR
+// O decorator roda toda vez que o método for executado
 function delayMessage(ms: number) {
-  // target: A classe
-  // key: O método
-  // PropertyDescriptor: Informações do método (configurable, enumerable, value, writable, get, set)
+  // target: A classe em si, o prototype
+  // key: O nome do método, string
+  // PropertyDescriptor: Informações do método
+  //    - configurable: costuma ser true
+  //    - enumerable: costuma ser true
+  //    - value: É a função em si, o método
+  //    - writable: costuma ser true
   return (target: any, key: string, descriptor: PropertyDescriptor) => {
+    // Salvando o método original, a função
     const originalMethod = descriptor.value
 
-    // @ts-ignore
-    descriptor.value = function (...args) {
+    // Sobreescrevendo o método original
+    descriptor.value = function (...args: any[]) {
       console.log(`Esperando ${ms}`)
       setTimeout(() => {
+        // Roda o método original com seus argumentos, caso tenha
         originalMethod.apply(this, args)
       }, ms)
 
+      // Retornando o description re-configurado
       return descriptor
     }
   }
@@ -103,7 +117,7 @@ class Greeter {
     this.greeting = g
   }
 
-  // Uma ideia boa é criar um @debounce para só quando usuário parar de digitar rodar um request
+  // Uma ideia boa é criar um @debounce para só quando usuário parar de digitar por um tempo rodar o request
   // Decorator que espera um tempo aí sim roda o método
   @delayMessage(1000)
   greet() {
@@ -111,4 +125,6 @@ class Greeter {
   }
 }
 const pessoinha = new Greeter('Pessoinha!')
+
+// O decorator delay deve rodar aqui
 pessoinha.greet()
